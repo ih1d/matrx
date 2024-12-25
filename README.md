be842270f8c47244314daca9ffa37a479e17accc
Isaac Hiram Lopez Diaz <isaac.lopez@upr.edu>
2024-12-25 12:28:59 -0500

    trying matrix mult

---
+++ README.md
@@ -0,0 +1,21 @@
+# Purely Functional Matrices*
+Native Haskell tensor library.
+<sup><sub>* This will be moved to tensors eventually</sup></sub>
+
+## No concern for alternative syntax
+```hs
+m1 :: Matrix a
+m1 = matrix 2 2 [1,2,3,4]
+[[1 2] [3 4]]
+```
+We use lists as main data structure for building so we do not rely on memorizing
+some syntax that will not satisfy every user. 
+
+## Basic operations
+transposition, addition, scalar-matrix multiplication, and matrix-matrix
+multiplication, row and column partitioning.
+
+## Integration with *vector* library
+It is most important that the library works well with already well-defined
+libraries such as *vector*. That is why no need of transformation between data
+structures is required.
--- src/Data/Matrix.hs
+++ src/Data/Matrix.hs
@@ -1,34 +1,36 @@
+{- Module      : Data.Matrix
+-- Copyright   : (c) Isaac H. Lopez Diaz 2024
+-- License     : BSD-style
+--
+-- Maintainer  : isaac.lopez@upr.edu
+-- Stability   : experimental
+-- Portability : non-portable
+-- 
+-- Native matrices
+-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Data.Matrix where
 
 -- Matrix data structure
--- needs a better api
 data Matrix a = Matrix 
     { row :: Int 
     , col :: Int 
     , elems :: Eq a => [[a]]
     }
 
+instance Semigroup (Matrix a) where
+    (<>) = undefined
+
+instance Monoid (Matrix a) where
+    mempty = Matrix 0 0 []
+    mappend = undefined
+
+{- Matrix Creation -}
 -- | empty matrix construction
 empty :: Matrix a
 empty = Matrix 0 0 []
-
--- | transposition
--- transposes the matrices elements
-transpose :: Matrix a -> Matrix a
-transpose (Matrix r c elems) = 
-    if r == 1 
-        then (Matrix c r elems)
-        else (Matrix c r (transposeIter elems []))
-    where 
-        transposeIter xs res =
-            let hds = map head xs
-                tls = map tail xs
-             in 
-                if concat tls == [] 
-                    then res ++ [hds]
-                    else transposeIter tls (res ++ [hds])
 
 -- | Given a row and a column with a list of
 -- elements a matrix data structure is returned.
@@ -45,37 +47,19 @@ matrix r c m
             loop _ [] = []
             loop n xs = [take n xs] ++ loop n (drop c xs) 
 
--- | dot product on matrices
-dot :: Matrix a -> Matrix a -> a
-dot (Matrix r1 c1 m1) (Matrix r2 c2 m2) = undefined
 
-saxpy :: Matrix a -> Matrix a -> a -> Matrix a
-saxpy m1 m2 a = undefined
-{-    case (col m1) /= (col m2) of
-        True -> error "saxpy needs to have same columns"
-        False -> saxpyIter m1 m2 a 0 []
-    where
-        saxpyIter x y alpha i ls =
-            if i == (col y) + 1
-                then Matrix (row y) (col y) ls
-                else 
-                    let xi = ((elems x) !! i) * alpha
-                        yi = (elems y) !! i
-                        yi' = yi + xi
-                     in saxpyIter x y alpha (i+1) (yi' : ls)
--}
-
-addition :: Matrix a -> Matrix a -> Matrix a
-addition _ _ = undefined
-
-multiplication :: Matrix a -> Matrix a -> Matrix a
-multiplication _ _ = undefined
-
-scalarMult :: a -> Matrix a -> Matrix a
-scalarMult _ _ = undefined
-
-pointwiseMult :: Matrix a -> Matrix a -> Matrix a
-pointwiseMult _ _ = undefined
-
-pointwiseDiv :: Matrix a -> Matrix a -> Matrix a
-pointwiseDiv _ _ = undefined
+-- | transposition
+-- transposes the matrices elements
+transpose :: Matrix a -> Matrix a
+transpose (Matrix r c elems) = 
+    if r == 1 
+        then (Matrix c r elems)
+        else (Matrix c r (transposeIter elems []))
+    where 
+        transposeIter xs res =
+            let hds = map head xs
+                tls = map tail xs
+             in 
+                if concat tls == [] 
+                    then res ++ [hds]
+                    else transposeIter tls (res ++ [hds])
